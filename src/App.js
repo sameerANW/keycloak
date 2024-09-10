44506787dcc9413c665e20adc8bfd0b771331752
sameeranw <sameerbasha.sk@appnetwise.com>
2024-09-10 13:49:15 +0530

    keycloak

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import keycloak from './keycloak';  // Import Keycloak
 
-function App() {
+const App = () => {
+
+  const handleLogin = () => {
+    keycloak.init({ onLoad: 'login-required' }).then(authenticated => {
+      if (!authenticated) {
+        keycloak.login();  // Redirect to Keycloak login
+      }
+    });
+  };
+
+  const handleSignup = () => {
+    keycloak.init({ onLoad: 'login-required' }).then(authenticated => {
+      if (!authenticated) {
+        keycloak.register();  // Redirect to Keycloak registration
+      }
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <img 
+        src="https://media.licdn.com/dms/image/D5616AQG9v1HWjdbknQ/profile-displaybackgroundimage-shrink_200_800/0/1684743884035?e=2147483647&v=beta&t=Zb-s7Cxb2zku8LVsPLId1IglY1rvfroAkBoIzYoU9hE" 
+        alt="Profile" 
+        className="profile-image"
+      />
+      <h1>Welcome to Appentwise Registration Form</h1>
+      <div>
+        <button onClick={handleLogin}>Sign In</button>
+        <button onClick={handleSignup}>Sign Up</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
